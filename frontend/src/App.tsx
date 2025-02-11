f1064757c433c174938aa04791662647764652cc
Junaid Syed <sjunaid626@gmail.com>
2025-02-11 22:13:02 +0530

    feat: add loading spinner to App component and adjust Navbar width

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,40 @@
-import Navbar from './components/Navbar'
-
-import Homepage from './pages/Homepage'
-import Work from './pages/Work'
-
-import './App.css'
-import About from './pages/About'
-
-import ContactPage from './pages/ContactPage'
-import Experience from './pages/Experience'
+import Navbar from './components/Navbar';
+import Homepage from './pages/Homepage';
+import Work from './pages/Work';
+import About from './pages/About';
+import ContactPage from './pages/ContactPage';
+import Experience from './pages/Experience';
+import { useEffect, useState } from 'react';
+import './App.css';
+import Spinner from './components/Spinner';
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+  }, []);
 
   return (
     <div className='flex flex-col bg-gray-100'>
       <Navbar />
-      <Homepage />
-      <Experience />
-      <Work />
-      <About />
-      <ContactPage />
+      {loading ? (
+        <div className="flex justify-center items-center h-screen">
+          <Spinner />
+        </div>
+      ) : (
+        <>
+          <Homepage />
+          <Experience />
+          <Work />
+          <About />
+          <ContactPage />
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
