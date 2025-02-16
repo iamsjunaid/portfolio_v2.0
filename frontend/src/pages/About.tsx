4f1ef809f30980df1835951027ce661ae66b1f92
Junaid Syed <sjunaid626@gmail.com>
2025-02-16 08:05:13 +0530

    fix: update resume link in About page to the correct URL

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -23,7 +23,7 @@ const About = () => {
                     Let's collaborate to turn your vision into a digital reality!
                 </p>
                 <Button asChild className="bg-gray-300 text-black rounded px-8 py-2 sm:px-12 sm:py-6 hover:text-white hover:border hover:border-white-950 transition-transform duration-300 ease-in-out text-sm">
-                    <a href="https://drive.google.com/file/d/1cZ7ykcpj5RER1zL_0XpxJXOqekF6h6tu/view?usp=sharing" className="flex items-center gap-2 group" target="_blank">
+                    <a href="https://drive.google.com/file/d/1NxOECikOd6zbwujjK5LqB_IhiGLjsr9D/view?usp=drive_link" className="flex items-center gap-2 group" target="_blank">
                         <span>My Resume</span>
                         <ArrowUpRight className="w-12 h-12 transition-transform duration-300 ease-in-out group-hover:rotate-45" />
                     </a>
