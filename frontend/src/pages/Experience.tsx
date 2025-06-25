c505e8f42a915062111faf30cb3601ee8b117b10
Junaid Syed <sjunaid626@gmail.com>
2025-06-25 14:31:33 +0530

    style: update background color of Experience section to transparent

--- frontend/src/pages/Experience.tsx
+++ frontend/src/pages/Experience.tsx
@@ -16,14 +16,14 @@ const itemVariants = {
 
 const Experience = () => {
     return (
-        <section className='bg-white rounded-bl-[8rem] px-4 py-16 sm:p-20 flex flex-col gap-4' id='experience'>
+        <section className='bg-transparent rounded-bl-[8rem] px-4 py-16 sm:p-20 flex flex-col gap-4' id='experience'>
             <p className='section-header'>Experience</p>
             <p className='text-gray-700 text-sm'>
                 I have worked on a variety of projects, ranging from web applications to mobile apps. My experience includes working with various technologies and frameworks, and I am always eager to learn new skills and improve my abilities.
             </p>
 
             <div className='sm:flex justify-around w-full h-full gap-8'>
-                <div className='flex flex-col gap-4 px-2 w-full'>
+                <div className='flex flex-col gap-4 w-full'>
                     {experiences.map((exp, index) => (
                         <motion.div
                             key={exp.role}
